e06bfcc7f143f5c8834ad8f7dfa7d02a300215b3
citren0 <cameron.davis1944@gmail.com>
2023-07-16 19:15:56 -0600

    Clock is accurate down to 1/10 ms

--- src/init.c
+++ src/init.c
@@ -5,13 +5,45 @@
 #include "regs.h"
 
 
+uint32_t cpuFreq;
+
+#define CPU240M 240000000
+
+
 
 int main(void);
 
 
-static inline void init(void) {
+static inline void fatal()
+{
+    while (1)
+    {
+        ;
+    }
+}
+
+
+static inline void setCPUFreq(uint32_t freq)
+{
+
+    cpuFreq = freq;
+
+    if (freq == CPU240M)
+    {
+        ((volatile uint32_t *)ESP32_DPORT)[15] |= 2UL << 0;
+    }
+    else
+    {
+        fatal();
+    }
+}
+
+
+static inline void init(void)
+{
 
     // Disable write protection on the RTC
+    // p716
     ((volatile uint32_t *)ESP32_RTCCNTL)[41] = 0x50d83aa1;
     
     // SW feeds the Watchdog Timer?
@@ -28,20 +60,24 @@ static inline void init(void) {
 
     // Enable TIMG0
     ((volatile uint32_t *)ESP32_TIMERGROUP0)[0] |= BIT(31);
+    // Timer auto increment
+    ((volatile uint32_t *)ESP32_TIMERGROUP0)[0] |= BIT(30);
+    // Timer multiple
+    ((volatile uint32_t *)ESP32_TIMERGROUP0)[0] |= (((uint64_t)0x00001)<<13);
 
     // Enable slow RTC clock speed
     ((volatile uint32_t *)ESP32_RTCCNTL)[28] |= 1UL << 27;
 
     // Select CPU Clock frequency
-    ((volatile uint32_t *)ESP32_DPORT)[15] |= 2UL << 0;
+    setCPUFreq(CPU240M);
 
     // Set UART frequency
     ((volatile uint32_t *)ESP32_UART0)[5] = 0x4001e0;
 
 }
 
-extern char _sbss, _ebss, _end, _eram;
 
+extern char _sbss, _ebss, _end, _eram;
 static char *s_heap_start, *s_heap_end, *s_brk;
 
 // Startup logic; this is the application entry point.
@@ -70,15 +106,14 @@ static uint64_t systick(void)
     // Give it a minute
     //asm volatile("nop");
 
-    // Read the value as a 64 bit integer which corresponds to 
+    // Read the value as a 64 bit integer which corresponds to the timer tick.
     return ((uint64_t) ((volatile uint32_t *)ESP32_TIMERGROUP0)[2] << 32) | ((volatile uint32_t *)ESP32_TIMERGROUP0)[1];
 }
 
 
 static uint64_t uptime_us(void)
 {
-    // systick returns in 32nds of a microsecond.
-    return systick() >> 5;
+    return (((systick() >> 5) * 6) / 5);
 }
 
 
@@ -100,7 +135,10 @@ static void delay_ms(unsigned long ms)
     delay_us(ms * 1000);
 }
 
+
 static void delay_s(unsigned long s)
 {
     delay_us(s * 1000000);
 }
+
+
